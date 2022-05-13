df1862f232cf72c3b14e53b9f210ba13a913a497
maham-ishtiaq-2000 <mahamishtiaq2000@gmail.com>
2022-05-13 06:21:53 +0500

    newOne

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const tailor = require('./Routes/tailor');
+const user = require('./Routes/user');
+const admin = require('./Routes/admin');
+const product = require('./Routes/products');
+const teacher = require('./Routes/teacher');
+const card = require('./Routes/card');
+const student = require('./Routes/student');
+const classes = require('./Routes/class');
+
+var corsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+app.set('view engine', 'ejs');
+// app.use('/uploads', express.static('/uploads'));
+// app.use(express.static(__dirname));
+app.use(cors(corsOptions));
+app.use(express.json());
+// var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/uploads'));
+
+app.use('/api/tailor', tailor);
+app.use('/api/user', user);
+app.use('/api/card',card)
+app.use('/api/admin', admin);
+app.use('/api/products', product);
+app.use('/api/teacher',teacher);
+app.use('/api/student',student);
+app.use('/api/classes',classes);
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://maham:maham2000@cluster0.5vxx9.mongodb.net/tinder-clone?retryWrites=true&w=majority",
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true })
+    .then(() => console.log("Connected to Gulaan...."))
+    .catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send("Welcome to Gulaan.....s")
+    console.log("ok")
+    // res.render('index');
+})
+
+
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log("listening on port no 8080");
+})
---
+++ models/class.js
@@ -0,0 +1,111 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+function classes_validation(classe) {
+    const joi_classes_schema = Joi.object
+        ({
+           
+            class_name: Joi.string().required().min(3).max(120),
+           
+            //images: Joi.string().min(2).max(1024),
+        })
+    return result = joi_classes_schema.validate(classe);
+}
+
+const classes_schema = new mongoose.Schema
+    ({
+    
+        class_name:
+        {
+            type: String,
+            required: true,
+            minlength: 3,
+            maxlength: 120,
+        },
+        my_students: [{
+            student_id: {
+                type: mongoose.Schema.Types.ObjectId
+            },
+            student_first_name:
+            {
+                type: String,
+                required: true,
+                minlength: 3,
+                maxlength: 120,
+            },
+            student_last_name:
+            {
+                type : String,
+                required : true,
+                minlength : 3,
+                maxlength : 120
+            }
+        }],
+        my_courses: [{
+            course_id: {
+                type: mongoose.Schema.Types.ObjectId
+            },
+            teacher_id:
+            {
+                type: mongoose.Schema.Types.ObjectId,
+            
+            },
+            name:
+            {
+                type : String,
+                required : true,
+                minlength : 3,
+                maxlength : 120
+            }
+        }],
+        class_quizes: [{
+            course_name: {
+                type: String,
+            },
+            class_name:
+            {
+                type: mongoose.Schema.Types.ObjectId,
+            
+            },
+            student_roll_number:
+            {
+                type : String,       
+             },
+            quiz_number :{
+                type : String,
+            },
+            total_marks:{
+                 type : String,
+            },
+            obtained_marks:{
+                type : String,
+            }
+        }],
+        class_mid_term: [{
+            course_name: {
+                type: String,
+            },
+            class_name:
+            {
+                type: mongoose.Schema.Types.ObjectId,
+            
+            },
+            student_roll_number:
+            {
+                type : String,       
+             },
+            total_marks:{
+                 type : String,
+            },
+            obtained_marks:{
+                type : String,
+            }
+        }],
+
+        
+    })
+
+const Class = mongoose.model('Class', classes_schema);
+module.exports.Class = Class;
+module.exports.classes_validation = classes_validation;
+
